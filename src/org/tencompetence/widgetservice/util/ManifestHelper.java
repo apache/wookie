7d69c2131ac2c6ad95a640208435a64a36bfd18a
ps3com <ps3com@unknown>
2007-12-14 20:27:36 +0000

    enabled password protection for the admin section
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wookie/trunk/trunk@827082 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/tencompetence/widgetservice/util/ManifestHelper.java
+++ src/org/tencompetence/widgetservice/util/ManifestHelper.java
@@ -20,18 +20,18 @@ import org.jdom.Namespace;
 import org.jdom.input.SAXBuilder;
 
 public class ManifestHelper implements IW3CXMLConfiguration {
-	
+
 	static Logger _logger = Logger.getLogger(ManifestHelper.class.getName());
-	
+
 	public static String[] dealWithManifest(String xmlText) throws JDOMException, IOException {
 		String title = "";
 		SAXBuilder builder = new SAXBuilder();
-		Element root = builder.build(new StringReader(xmlText)).getRootElement();			
-		if(root.getName().equalsIgnoreCase(WIDGET_ELEMENT)){				
+		Element root = builder.build(new StringReader(xmlText)).getRootElement();
+		if(root.getName().equalsIgnoreCase(WIDGET_ELEMENT)){
 			String id = root.getAttributeValue(ID_ATTRIBUTE);
 			String start = root.getAttributeValue(START_ATTRIBUTE);
 			String height = root.getAttributeValue(HEIGHT_ATTRIBUTE);
-			String width = root.getAttributeValue(WIDTH_ATTRIBUTE);														
+			String width = root.getAttributeValue(WIDTH_ATTRIBUTE);
 			Element titleElement = root.getChild(TITLE_ELEMENT, Namespace.getNamespace(MANIFEST_NAMESPACE));
 			if(titleElement==null){
 				title = "Unnamed Widget";
@@ -49,22 +49,22 @@ public class ManifestHelper implements IW3CXMLConfiguration {
 			return null;
 		}
 	}
-	
+
 	public static File createUnpackedWidgetFolder(HttpServletRequest request, Configuration properties, String folder) throws IOException{
 		folder = convertIdToFolderName(folder);
 		String uploadPath = properties.getString("widget.widgetfolder");
-		ServletContext context = request.getSession().getServletContext();		
-		String serverPath = context.getRealPath(uploadPath + File.separator + folder) ;		
+		ServletContext context = request.getSession().getServletContext();
+		String serverPath = context.getRealPath(uploadPath + File.separator + folder) ;
 		File file = new File(convertPathToPlatform(serverPath));
 		return file;
 	}
-	
-	public static String getURLForWidget(Configuration properties, String folder, String file){	
+
+	public static String getURLForWidget(Configuration properties, String folder, String file){
 		folder = convertIdToFolderName(folder);
-		String path = convertPathToRelativeUri("/wookie" + properties.getString("widget.widgetfolder") + File.separator + folder + File.separator + file);		
+		String path = convertPathToRelativeUri("/wookie" + properties.getString("widget.widgetfolder") + File.separator + folder + File.separator + file);
 		return path;
 	}
-	
+
 	public static String convertIdToFolderName(String folder){
 		if(folder.startsWith("http://")){
 			folder = folder.substring(7, folder.length());
@@ -72,16 +72,16 @@ public class ManifestHelper implements IW3CXMLConfiguration {
 		folder.replaceAll(" ", "");
 		return folder;
 	}
-	
+
 	public static File dealWithUploadFile(HttpServletRequest request, Configuration properties) throws Exception {
 		File uFile = null;
 		String uploadPath = properties.getString("widget.useruploadfolder");
-		ServletContext context = request.getSession().getServletContext();		
+		ServletContext context = request.getSession().getServletContext();
 		String serverPath = convertPathToPlatform(context.getRealPath(uploadPath));
-		_logger.debug(serverPath);		
+		_logger.debug(serverPath);
 		String archiveFileName = null;
 			if (FileUploadBase.isMultipartContent(request)) {
-				_logger.info("uploading file...");
+				_logger.debug("uploading file...");
 				DiskFileUpload fu = new DiskFileUpload();
 				// maximum size before a FileUploadException will be thrown
 				fu.setSizeMax(1024 * 1024 * 1024);
@@ -96,25 +96,25 @@ public class ManifestHelper implements IW3CXMLConfiguration {
 					Iterator<?> i = fileItems.iterator();
 					FileItem fi = (FileItem) i.next();
 					File file = new File(convertPathToPlatform(fi.getName()));
-					archiveFileName = file.getName();					
-					
+					archiveFileName = file.getName();
+
 					uFile = new File(serverPath + File.separator + archiveFileName);
-					
+
 					fi.write(uFile);
 					_logger.debug("Upload completed successfully ["
 							+ archiveFileName + "]-"
-							+ (fi.isInMemory() ? "M" : "D"));															
+							+ (fi.isInMemory() ? "M" : "D"));
 				}
-			}		
+			}
 		return uFile;
-		
+
 	}
-	
-	
+
+
 	public static String convertPathToRelativeUri(String path){
-		return path.replace('\\', '/');		
+		return path.replace('\\', '/');
 	}
-	
+
 	  public static String convertPathToPlatform(String path) {
 		String result = path.replace('\\', '/')
 				.replace('/', File.separatorChar);
@@ -123,22 +123,22 @@ public class ManifestHelper implements IW3CXMLConfiguration {
 		}
 		return result;
 	}
-	  
+
 	 public static boolean removeWidgetResources(HttpServletRequest request, Configuration properties, String folder){
 		folder = convertIdToFolderName(folder);
 		String uploadPath = properties.getString("widget.widgetfolder");
-		ServletContext context = request.getSession().getServletContext();		
-		String serverPath = context.getRealPath(uploadPath + File.separator + folder) ;		
+		ServletContext context = request.getSession().getServletContext();
+		String serverPath = context.getRealPath(uploadPath + File.separator + folder) ;
 		File pFolder = new File(convertPathToPlatform(serverPath));
 		try {
 			_logger.debug("Deleting folder:"+pFolder.getCanonicalFile().toString());
 			FileUtils.deleteFolder(pFolder);
-		} 
+		}
 		catch (Exception ex) {
-			_logger.error(ex);						
+			_logger.error(ex);
 		}
 		return true;
 	 }
-	 
+
 
 }
