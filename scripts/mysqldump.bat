e8cb612b971ec9b28af02a47b130d7b2d6c9db32
ps3com <ps3com@unknown>
2008-03-11 16:06:47 +0000

    updated paths for me
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wookie/trunk/trunk@827110 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/mysqldump.bat
+++ scripts/mysqldump.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
 REM -u root -p admin 
 
-SET MYSQL="E:\databases\mysql5.0\bin\mysql"
-SET MYSQLDUMP="E:\databases\mysql5.0\bin\mysqldump"
+SET MYSQL="D:\Program Files\MySQL\MySQL Server 5.0\bin\mysql"
+SET MYSQLDUMP="D:\Program Files\MySQL\MySQL Server 5.0\bin\mysqldump"
 SET MYSQL_PORT=3306
 SET MYSQL_USER=root
-SET MYSQL_PWD=admin
+SET MYSQL_PWD=isbn0471
 SET DB_NAME=widgetDB
 
 REM *** DUMP DATABASE ***
