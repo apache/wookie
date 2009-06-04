4d588b54d41a7b82a96ffbe5b66dfafac97c31b2
ps3com <ps3com@unknown>
2009-06-04 15:11:48 +0000

    added readonly attribute
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wookie/trunk/trunk@827349 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/tencompetence/widgetservice/beans/PreferenceDefault.java
+++ src/org/tencompetence/widgetservice/beans/PreferenceDefault.java
@@ -1,13 +1,36 @@
-/**
- * 
+/*
+ * Copyright (c) 2007, Consortium Board TENCompetence
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the TENCompetence nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY CONSORTIUM BOARD TENCOMPETENCE ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL CONSORTIUM BOARD TENCOMPETENCE BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.tencompetence.widgetservice.beans;
 
 import java.util.Map;
 
-/**
- * @author scott
- *
+/** 
+ * @author Scott Wilson
+ * @version $Id: PreferenceDefault.java,v 1.3 2009-06-04 15:11:48 ps3com Exp $ 
  */
 public class PreferenceDefault extends AbstractKeyBean<PreferenceDefault> {
 
@@ -16,7 +39,8 @@ public class PreferenceDefault extends AbstractKeyBean<PreferenceDefault> {
 	private Widget widget;
 	private String preference;
 	private String value;
-
+	private boolean readOnly;
+	
 	public Widget getWidget(){
 		return widget;
 	}
@@ -48,6 +72,14 @@ public class PreferenceDefault extends AbstractKeyBean<PreferenceDefault> {
 	 */
 	public void setValue(String value) {
 		this.value = value;
+	}
+	
+	public boolean isReadOnly() {
+		return readOnly;
+	}
+
+	public void setReadOnly(boolean readOnly) {
+		this.readOnly = readOnly;
 	}
 	
 	/// Active record methods
