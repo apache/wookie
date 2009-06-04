44cb698bd143c3ee95cc5f3cf72579697b0aec98
ps3com <ps3com@unknown>
2009-06-04 14:59:51 +0000

    added new tables for features & params & ServerFeatures
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wookie/trunk/trunk@827333 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/widgetdb_mysql.sql
+++ scripts/widgetdb_mysql.sql
@@ -40,7 +40,9 @@ CREATE TABLE `PreferenceDefault` (
     `widget_id` int(11) NOT NULL,
     `preference` varchar(255) NOT NULL,
     `value` varchar(1024) NOT NULL,
+    `readOnly` char(1) default 'f',
     PRIMARY KEY(`id`),
+    KEY `FK_preferencesdefault_1` (`widget_id`),
     CONSTRAINT `FK_preferencesdefault_1` FOREIGN KEY (`widget_id`) REFERENCES `Widget` (`id`)
 );
 
@@ -123,9 +125,38 @@ CREATE TABLE `Post` (
   PRIMARY KEY  (`id`)
 );
 
+CREATE TABLE `Feature` (
+	`id` int(11) NOT NULL auto_increment,
+	`featureName` varchar(255) NOT NULL,
+	`required` char(1) default 'T',
+	`widget_id` int(11) NOT NULL,
+	PRIMARY KEY  (`id`),
+	KEY `FKEASDD9E2C2E1E5` (`widget_id`),
+	CONSTRAINT `FKEASDD9E2C2E1E5` FOREIGN KEY (`widget_id`) REFERENCES `Widget` (`id`)
+);
 
-INSERT INTO `Widget` VALUES (1,'Unsupported widget widget','/wookie/wservices/www.tencompetence.org/widgets/default/notsupported/index.htm',350,500,'f','http://www.tencompetence.org/widgets/default/notsupported','This widget is a placeholder for when no corresponding widget is found for a given type','Paul Sharples','/wookie/shared/images/defaultwidget.png'),(2,'Default chat widget','/wookie/wservices/www.tencompetence.org/widgets/default/chat/index.htm',358,500,'f','http://www.tencompetence.org/widgets/default/chat','This widget provides a simple chat or Instant messaging facility','Paul Sharples','/wookie/shared/images/chat.png'),(3,'Default discussion/forum widget','/wookie/wservices/www.tencompetence.org/widgets/default/forum/index.htm',350,520,'t','http://www.tencompetence.org/widgets/default/forum','This widget provides a threaded discussion forum facility','Paul Sharples','/wookie/shared/images/forum.png'),(4,'Default vote widget','/wookie/wservices/www.tencompetence.org/widgets/default/vote/index.htm',350,500,'f','http://www.tencompetence.org/widgets/default/vote','This widget provides a voting facility','Paul Sharples','/wookie/shared/images/vote.png'),(5,'Natter','/wookie/wservices/www.getwookie.org/widgets/natter/index.htm',383,255,'F','http://www.getwookie.org/widgets/natter','A simple chat widget','Scott Wilson','/wookie/wservices/www.getwookie.org/widgets/natter/Icon.png'),(6,'Weather','/wookie/wservices/www.getwookie.org/widgets/weather/index.htm',125,125,'F','http://www.getwookie.org/widgets/weather','A simple weather widget','Scott Wilson','/wookie/wservices/www.getwookie.org/widgets/weather/icon.png');
+CREATE TABLE `Param` (
+  `id` int(11) NOT NULL auto_increment,
+  `feature_id` int(11) NOT NULL,
+  `parameterName` varchar(255) NOT NULL,
+  `parameterValue` varchar(255) NOT NULL,  
+  PRIMARY KEY  (`id`),
+  KEY `FKAA9137E222222` (`feature_id`),
+  CONSTRAINT `FKAA9137E222222` FOREIGN KEY (`feature_id`) REFERENCES `Feature` (`id`)
+);
+
+CREATE TABLE `ServerFeature` (
+	`id` int(11) NOT NULL auto_increment,
+	`featureName` varchar(255) NOT NULL,
+	PRIMARY KEY  (`id`)
+);
+
+
+
+INSERT INTO `Widget` VALUES (1,'Unsupported widget widget','/wookie/wservices/www.tencompetence.org/widgets/default/notsupported/index.htm',350,500,'f','http://www.tencompetence.org/widgets/default/notsupported','This widget is a placeholder for when no corresponding widget is found for a given type','Paul Sharples','/wookie/shared/images/defaultwidget.png', 'v1.0'),(2,'Default chat widget','/wookie/wservices/www.tencompetence.org/widgets/default/chat/index.htm',358,500,'f','http://www.tencompetence.org/widgets/default/chat','This widget provides a simple chat or Instant messaging facility','Paul Sharples','/wookie/shared/images/chat.png', 'v1.0'),(3,'Default discussion/forum widget','/wookie/wservices/www.tencompetence.org/widgets/default/forum/index.htm',350,520,'t','http://www.tencompetence.org/widgets/default/forum','This widget provides a threaded discussion forum facility','Paul Sharples','/wookie/shared/images/forum.png', 'v1.0'),(4,'Default vote widget','/wookie/wservices/www.tencompetence.org/widgets/default/vote/index.htm',350,500,'f','http://www.tencompetence.org/widgets/default/vote','This widget provides a voting facility','Paul Sharples','/wookie/shared/images/vote.png', 'v1.0'),(5,'Natter','/wookie/wservices/www.getwookie.org/widgets/natter/index.htm',383,255,'F','http://www.getwookie.org/widgets/natter','A simple chat widget','Scott Wilson','/wookie/wservices/www.getwookie.org/widgets/natter/Icon.png', 'v1.0'),(6,'Weather','/wookie/wservices/www.getwookie.org/widgets/weather/index.htm',125,125,'F','http://www.getwookie.org/widgets/weather','A simple weather widget','Scott Wilson','/wookie/wservices/www.getwookie.org/widgets/weather/icon.png', 'v1.0');
 INSERT INTO `WidgetDefault` VALUES ('unsupported',1),('chat',2),('discussion',3),('forum',3),('vote',4),('natter',5),('weather',6);
 INSERT INTO `WidgetService` VALUES (1,'unsupported'),(2,'chat'),(3,'discussion'),(4,'forum'),(5,'vote'),(6,'natter'),(7,'weather');
 INSERT INTO `WidgetType` VALUES (1,1,'unsupported'),(2,2,'chat'),(3,3,'forum'),(4,3,'discussion'),(5,4,'vote'),(6,5,'natter'),(7,6,'weather');
 INSERT INTO `Whitelist` VALUES (1,'http://127.0.0.1'),(2,'http://localhost'),(3,'http://feeds.bbc.co.uk/weather/feeds/rss');
+INSERT INTO `ServerFeature` VALUES (1,'polling');
+INSERT INTO `ApiKey` VALUES (1,'TEST','test@127.0.0.1');
