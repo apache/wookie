a4b797aad16183988d7ef44dfdc24e2df1e61f10
scottwilson <scottwilson@unknown>
2009-07-28 16:05:23 +0000

    Applied Apache 2.0 license
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wookie/trunk/trunk@827411 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/wookie/util/RandomGUID.java
+++ src/org/apache/wookie/util/RandomGUID.java
@@ -1,3 +1,18 @@
+/*
+ *  (c) 2009 Marc A Mnich
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.wookie.util;
 
 import java.net.InetAddress;
@@ -92,7 +107,7 @@ import java.util.Random;
  * - Marc
  */
 /**
-* @version $Id: RandomGUID.java,v 1.1 2009-07-22 09:31:29 scottwilson Exp $
+* @version $Id: RandomGUID.java,v 1.2 2009-07-28 16:05:21 scottwilson Exp $
 */
 public class RandomGUID {
 	
