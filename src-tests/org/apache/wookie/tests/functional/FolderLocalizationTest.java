6f6e9b8bb4916a88b08a0888a4cf4cca855791ac
Scott Wilson <scottbw@apache.org>
2010-01-02 21:57:35 +0000

    Fix for WOOKIE-94 (part of WOOKIE-67) implementing folder-based localization using a servlet filter. Includes test cases. Note that this does slow down a Wookie server as all static resource requests get intercepted, so we should also look into caching or other performance optimisation methods for this solution.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wookie/trunk@895301 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src-tests/org/apache/wookie/tests/functional/FolderLocalizationTest.java
@@ -0,0 +1,152 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wookie.tests.functional;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.cookie.CookiePolicy;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.lang.StringUtils;
+import org.jdom.Document;
+import org.jdom.input.SAXBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+/**
+ * Tests folder-based localization using the localetest widget
+ * @author scott
+ *
+ */
+public class FolderLocalizationTest extends AbstractControllerTest {
+	
+	private static final String LOCALIZED_WIDGET = "http://www.getwookie.org/widgets/localetest";
+	private static String WIDGET_START_URL_ROOT;
+	protected static final String SERVER_URL = "http://localhost:8080";
+	
+	private static HttpClient client;
+	private static GetMethod get;
+	
+	@BeforeClass
+	public static void setup(){
+		// Set up the client and enable cookies
+        client = new HttpClient();
+        client.getParams().setCookiePolicy(CookiePolicy.RFC_2109);
+        get = new GetMethod();
+        
+        // Create a widget instance localized to Yorkshire English
+        try {
+	        PostMethod post = new PostMethod(TEST_INSTANCES_SERVICE_URL_VALID);
+	        post.setQueryString("api_key="+API_KEY_VALID+"&widgetid="+LOCALIZED_WIDGET+"&userid=foldertest1&shareddatakey=foldertest1&locale=en-gb-yorks");
+	        client.executeMethod(post);
+	      
+	        WIDGET_START_URL_ROOT = getStartFile(post.getResponseBodyAsString());
+	        // We have to load the start file in order to start the session
+	        getResource(WIDGET_START_URL_ROOT);
+	        // take off the resource bit
+	        String path = WIDGET_START_URL_ROOT.substring(WIDGET_START_URL_ROOT.indexOf("locales"));
+	        WIDGET_START_URL_ROOT= StringUtils.remove(WIDGET_START_URL_ROOT, path);
+	        post.releaseConnection();
+	    }
+	    catch (Exception e) {
+	    	e.printStackTrace();
+	    }			
+	}
+	
+	/**
+	 * Gets the start file url for an instance
+	 * @param response
+	 * @return
+	 */
+	private static String getStartFile(String response){
+		SAXBuilder builder = new SAXBuilder();
+		Reader in = new StringReader(response);
+		Document doc;
+		try {
+			doc = builder.build(in);
+		} catch (Exception e) {
+			return null;
+		} 		
+		return doc.getRootElement().getChild("url").getText();
+	}
+	
+	/**
+	 * Gets a resource and returns the final URL after redirection
+	 * @param url
+	 * @return
+	 */
+	private static String getResource(String url){
+		get = new GetMethod(url);
+		get.setFollowRedirects(true);
+		try {
+			client.executeMethod(get);
+			return SERVER_URL + get.getPath();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return null;
+	}
+	
+	@Test
+	// The root resource is overridden by the EN-GB-YORKS localized resource
+	public void getLocalizedResourceFromRootPath(){
+		String resource = "index.htm";
+		String url = WIDGET_START_URL_ROOT + resource;
+		assertEquals(WIDGET_START_URL_ROOT+"locales/en-gb-yorks/"+resource, getResource(url));
+	}
+	
+	@Test
+	// The root resource is correct, so should not be redirected
+	public void getRootResourceFromRootResource(){
+		String resource = "Images/test.png";
+		String url = WIDGET_START_URL_ROOT + resource;
+		assertEquals(WIDGET_START_URL_ROOT+resource, getResource(url));	
+	}
+	
+	@Test
+	// The FR resource is replaced by the Root resource as there is no EN resource
+	public void getRootResourceFromIncorrectLocalizedPath(){
+		String resource = "locales/fr/Images/test.png";
+		String url = WIDGET_START_URL_ROOT + resource;
+		assertEquals(WIDGET_START_URL_ROOT+"Images/test.png", getResource(url));
+	}
+	
+	@Test
+	// The EN resource is replaced by the Root resource as there is no EN resource
+	public void getRootResourceFromLocalizedPath(){
+		String resource = "locales/en/Images/test.png";
+		String url = WIDGET_START_URL_ROOT + resource;
+		assertEquals(WIDGET_START_URL_ROOT+"Images/test.png", getResource(url));
+	}
+	
+	@Test
+	// The FR resource is replaced by the EN-GB-YORKS resource
+	public void getLocalizedResourceFromIncorrectLocalizedPath(){
+		String resource = "locales/fr/index.htm";
+		String url = WIDGET_START_URL_ROOT + resource;
+		assertEquals(WIDGET_START_URL_ROOT+"locales/en-gb-yorks/index.htm", getResource(url));
+	}
+	
+	@Test
+	// The EN-GB-YORKS resource is not available and is replaced by the EN resource
+	public void getLocalizedResourceGracefulDegradation(){
+		String resource = "locales/en-gb-yorks/test.txt";
+		String url = WIDGET_START_URL_ROOT + resource;
+		assertEquals(WIDGET_START_URL_ROOT+"locales/en/test.txt", getResource(url));
+	}
+
+}
---
+++ src/org/apache/wookie/server/LocalizedResourceFilter.java
@@ -0,0 +1,181 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.wookie.server;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.wookie.beans.WidgetInstance;
+import org.apache.wookie.util.LocalizationUtils;
+import org.apache.wookie.util.WidgetPackageUtils;
+
+import com.ibm.icu.util.ULocale;
+
+/**
+ * A Filter for implementing rules for folder-based localization. Redirects requests
+ * for widget resources to resources in appropriate locale folders.
+ * 
+ * Note that this relies on servlet session management, which relies on cookies being
+ * enabled in the browser.
+ * 
+ * This filter runs for a lot of what are otherwise completely static resources, so its a real 
+ * drag on server performance
+ */
+public class LocalizedResourceFilter implements Filter {
+
+	private FilterConfig filterConfig = null;
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+		// If I have an instance key in the query, set it in the session
+		// This will only happen if the resource is the Start File for the Widget
+		String idkey = ((HttpServletRequest)request).getParameter("idkey");
+		// We use the "localized" querystring parameter to indicate a resource
+		// has already been processed by this algorithm once, to prevent hunting and
+		// infinite recursion
+		String localized = ((HttpServletRequest)request).getParameter("localized");
+		
+		if (idkey!=null){
+			filterConfig.getServletContext().setAttribute("id_key", idkey);
+		} else {
+			// Skip if already localized
+			if (localized == null){
+				// Find the instance key in the current session
+				String key = (String)filterConfig.getServletContext().getAttribute("id_key");
+				if (key != null){
+					WidgetInstance instance = WidgetInstance.findByIdKey(key);
+					if (instance != null){
+						// Only if we have a valid instance and a resource which has no localization
+						// parameter do we start the locale algorithm
+
+						// Get the original request URL
+						String uri = ((HttpServletRequest)request).getRequestURL().toString();
+						URL url = new URL(uri);
+						String path = url.getPath();
+
+						// Process the resource with the localization algorithm
+						String localizedPath = getLocalizedResource(path, instance);
+
+						// Redirect to localized resource URL only if different from the original resource URL
+						if (!path.equals(localizedPath)){
+							uri = uri.replace(path, localizedPath);
+							if (uri.contains("?")){
+								uri += "&localized=1";
+							} else {
+								uri += "?localized=1";
+							}
+							URL newUrl = new URL(uri);
+							((HttpServletResponse)response).sendRedirect(newUrl.toString());
+							return;							
+						}
+					}
+				}
+			}
+		}
+		chain.doFilter(request, response); 
+	}
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.filterConfig = filterConfig;
+	}
+
+	/**
+	 * Process a resource path in the context of a widget instance, identify 
+	 * the path for any resource which matches the path in a locale folder
+	 * which matches the locale preference for the widget instance.
+	 * 
+	 * If no locale preference is set for the instance, the default server locale
+	 * is used.
+	 * 
+	 * If no localized path can be determined, the method returns the original path supplied.
+	 * 
+	 * @param originalPath
+	 * @param instance
+	 * @return the localized path for the resource; if none is found, the original path is returned
+	 */
+	private String getLocalizedResource(String originalPath, WidgetInstance instance){
+		// Remove the context URI from the path, or this will mess up
+		// the algorithm for locating the file using its real path
+		String context = filterConfig.getServletContext().getContextPath();
+		String basePath = originalPath.substring(originalPath.indexOf(context)+context.length());
+
+		// Strip out the widget base path to obtain just the resource itself
+		String widgetPath = getWidgetInstanceBasePath(instance);
+		int start = originalPath.indexOf(widgetPath)+widgetPath.length();
+		String resource = originalPath.substring(start);
+
+		// If the resource already contains a locale, remove it for the purposes of searching
+		// This is because local links witin widgets can target localized files - e.g.
+		// In the example widget below, there will be a link from locales/es/index.html to "locales/es/images/test.png"
+		// but we need to consider this as a request for /images/test.png
+		// 
+		// index.html
+		// /images/test.png
+		// /locales/es/index.html
+		//
+		if (resource.startsWith("locales/")){
+			String[] parts = resource.split("/");
+			String prefix = "/"+parts[0]+"/"+parts[1]+"/";
+			String nonLocalResource = resource.substring(resource.indexOf(prefix)+prefix.length());
+			basePath = basePath.replace(resource, nonLocalResource);
+			resource = nonLocalResource;
+		}
+
+		// For each locale in the list see if a matching localized resource exists.
+		// If it does, return it and terminate this algorithm
+		List<ULocale> locales = LocalizationUtils.getProcessedLocaleList(new String[]{instance.getLang()});
+		for (ULocale locale:locales){
+			String path = basePath.replace(resource, "locales/"+locale.toLanguageTag()+"/"+resource);
+			String filePath = filterConfig.getServletContext().getRealPath(path);
+			if (new File(filePath).exists()) return context+path;
+		}
+
+		// All attempts to locate a localized copy have failed, so we must try to find a non-localized version instead
+		if (new File(filterConfig.getServletContext().getRealPath(basePath)).exists()) return context+basePath;
+
+		// No localized or even non-localized file exists, so just return the original. This situation shouldn't arise except
+		// where, e.g., the original request was for a non-existing resource
+		return originalPath;
+	}
+
+	/**
+	 * Return the base path for a widget instance
+	 * @param instance
+	 * @return
+	 */
+	private String getWidgetInstanceBasePath(WidgetInstance instance){
+		String guid = instance.getWidget().getGuid();
+		Configuration config = (Configuration) filterConfig.getServletContext().getAttribute("properties");
+		final String localWidgetFolderPath = config.getString("widget.widgetfolder");
+		return WidgetPackageUtils.getURLForWidget(localWidgetFolderPath, guid, "");
+	}
+
+}
