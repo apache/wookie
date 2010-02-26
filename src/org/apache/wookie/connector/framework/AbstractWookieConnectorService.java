c23c2964fb74c71d4a6f2a93526be6afeb14f3f2
Ross Duncan Gardler <rgardler@apache.org>
2010-02-26 12:04:27 +0000

    @whitespace clean imports
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wookie/trunk@916656 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/wookie/connector/framework/AbstractWookieConnectorService.java
+++ src/org/apache/wookie/connector/framework/AbstractWookieConnectorService.java
@@ -3,7 +3,6 @@ package org.apache.wookie.connector.framework;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
