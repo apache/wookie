1ea5168ccc627fc5c60f7ca545492d447ba733ce
Ross Duncan Gardler <rgardler@apache.org>
2010-03-03 18:15:46 +0000

    Fix license header comments (thanks to Raido Kuli)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wookie/trunk@918610 13f79535-47bb-0310-9956-ffa450edef68

--- connector/php/TestWookieService.php
+++ connector/php/TestWookieService.php
@@ -1,5 +1,5 @@
-<%--
-/*
+<?php
+/**
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -12,8 +12,6 @@
  *  See the License for the specific language governing permissions and
  * limitations under the License.
  */
---%>
-<?php
 
 //test service
 ini_set('display_errors', 1);
