60e77ec2271b9b263c5d16612e792255bf595ccc
Scott Wilson <scottbw@apache.org>
2010-10-06 13:08:35 +0000

    Replaced with the latest version of the Sudoku generator, the main difference is that this version is explicitly licensed under the ASF 2.0 License.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wookie/trunk@1005012 13f79535-47bb-0310-9956-ffa450edef68

--- widgets/sudoku/sudoku.js
+++ widgets/sudoku/sudoku.js
@@ -1,12 +1,15 @@
-// Copyright 2007 Google Inc.
-// All Rights Reserved.
-
-/**
- * @fileoverview sudoku.js is a library of of classes and utilies that generate
- * a solvable sudoku puzzle as a string of 81 numbers - with '0' to denote a 
- * a blank cell.
- * @author austinchau@google.com (Austin Chau)
- */
+// Copyright [2007] [Austin Chau] 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not 
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at 
+//
+// http://www.apache.org/licenses/LICENSE-2.0 
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+// License for the specific language governing permissions and limitations 
+// under the License.
 
 // Namespace assigned to this library of sudoku-related classes
 var sudoku = {};
@@ -435,7 +438,7 @@ sudoku.SudokuGenerator.prototype.toArray = function() {
  */      
 sudoku.SudokuGenerator.prototype.createPartialArray = function() {    
   this.partialArray = this.fullArray.slice();    
-  var emptySymbol = 0;
+  var emptySymbol = '0';
     
   var scratch = null;    
   var minRemove = 3;
