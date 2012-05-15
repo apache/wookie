eb7cc4e85ca724c23134dc5290b1d1f1352702bb
Scott Wilson <scottbw@apache.org>
2012-05-15 11:16:01 +0000

    Rolling forward version to 0.11.0-SNAPSHOT in trunk
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wookie/trunk@1338650 13f79535-47bb-0310-9956-ffa450edef68

--- etc/release/runsignatures.sh
+++ etc/release/runsignatures.sh
@@ -11,69 +11,69 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set WOOKIE-VERSION=0.10.0-incubating
-set ROOT-FOLDER=%WOOKIE-VERSION%
-set SOURCE-FOLDER=source
-set SRC-BUILD-TAR=apache-wookie-%WOOKIE-VERSION%-src.tar.gz
-set SRC-BUILD-ZIP=apache-wookie-%WOOKIE-VERSION%-src.zip
-set BINARY-FOLDER-STANDALONE=binary/standalone
-set BINARY-STANDALONE-TAR=apache-wookie-%WOOKIE-VERSION%-standalone.tar.gz
-set BINARY-STANDALONE-ZIP=apache-wookie-%WOOKIE-VERSION%-standalone.zip
-set BINARY-FOLDER-WAR=binary/war
-set BINARY-WAR-TAR=apache-wookie-%WOOKIE-VERSION%-war.tar.gz
-set BINARY-WAR-ZIP=apache-wookie-%WOOKIE-VERSION%-war.zip
+export WOOKIE-VERSION=0.11.0-incubating-SNAPSHOT
+export ROOT-FOLDER=$WOOKIE-VERSION$
+export SOURCE-FOLDER=source
+export SRC-BUILD-TAR=apache-wookie-$WOOKIE-VERSION$-src.tar.gz
+export SRC-BUILD-ZIP=apache-wookie-$WOOKIE-VERSION$-src.zip
+export BINARY-FOLDER-STANDALONE=binary/standalone
+export BINARY-STANDALONE-TAR=apache-wookie-$WOOKIE-VERSION$-standalone.tar.gz
+export BINARY-STANDALONE-ZIP=apache-wookie-$WOOKIE-VERSION$-standalone.zip
+export BINARY-FOLDER-WAR=binary/war
+export BINARY-WAR-TAR=apache-wookie-$WOOKIE-VERSION$-war.tar.gz
+export BINARY-WAR-ZIP=apache-wookie-$WOOKIE-VERSION$-war.zip
 
-cd %ROOT-FOLDER%/%SOURCE-FOLDER%
-gpg2 --armor --output %SRC-BUILD-TAR%.asc --detach-sig %SRC-BUILD-TAR%
-echo verifying the signature for %SRC-BUILD-TAR%
-gpg2 --verify %SRC-BUILD-TAR%.asc %SRC-BUILD-TAR%
-echo Creating MD5 value for %SRC-BUILD-TAR%
-gpg2 --print-md MD5 %SRC-BUILD-TAR% > %SRC-BUILD-TAR%.md5
-echo Creating SHA512 value for %SRC-BUILD-TAR%
-gpg2 --print-md SHA512 %SRC-BUILD-TAR% > %SRC-BUILD-TAR%.sha
+cd $ROOT-FOLDER$/$SOURCE-FOLDER$
+gpg2 --armor --output $SRC-BUILD-TAR$.asc --detach-sig $SRC-BUILD-TAR$
+echo verifying the signature for $SRC-BUILD-TAR$
+gpg2 --verify $SRC-BUILD-TAR$.asc $SRC-BUILD-TAR$
+echo Creating MD5 value for $SRC-BUILD-TAR$
+gpg2 --print-md MD5 $SRC-BUILD-TAR$ > $SRC-BUILD-TAR$.md5
+echo Creating SHA512 value for $SRC-BUILD-TAR$
+gpg2 --print-md SHA512 $SRC-BUILD-TAR$ > $SRC-BUILD-TAR$.sha
 
-gpg2 --armor --output %SRC-BUILD-ZIP%.asc --detach-sig %SRC-BUILD-ZIP%
-echo verifying the signature for %SRC-BUILD-ZIP%
-gpg2 --verify %SRC-BUILD-ZIP%.asc %SRC-BUILD-ZIP%
-echo Creating MD5 value for %SRC-BUILD-ZIP%
-gpg2 --print-md MD5 %SRC-BUILD-ZIP% > %SRC-BUILD-ZIP%.md5
-echo Creating SHA512 value for %SRC-BUILD-ZIP%
-gpg2 --print-md SHA512 %SRC-BUILD-ZIP% > %SRC-BUILD-ZIP%.sha
+gpg2 --armor --output $SRC-BUILD-ZIP$.asc --detach-sig $SRC-BUILD-ZIP$
+echo verifying the signature for $SRC-BUILD-ZIP$
+gpg2 --verify $SRC-BUILD-ZIP$.asc $SRC-BUILD-ZIP$
+echo Creating MD5 value for $SRC-BUILD-ZIP$
+gpg2 --print-md MD5 $SRC-BUILD-ZIP$ > $SRC-BUILD-ZIP$.md5
+echo Creating SHA512 value for $SRC-BUILD-ZIP$
+gpg2 --print-md SHA512 $SRC-BUILD-ZIP$ > $SRC-BUILD-ZIP$.sha
 
-cd ../%BINARY-FOLDER-STANDALONE%
+cd ../$BINARY-FOLDER-STANDALONE$
 
-gpg2 --armor --output %BINARY-STANDALONE-TAR%.asc --detach-sig %BINARY-STANDALONE-TAR%
-echo verifying the signature for %BINARY-STANDALONE-TAR%
-gpg2 --verify %BINARY-STANDALONE-TAR%.asc %BINARY-STANDALONE-TAR%
-echo Creating MD5 value for %BINARY-STANDALONE-TAR%
-gpg2 --print-md MD5 %BINARY-STANDALONE-TAR% > %BINARY-STANDALONE-TAR%.md5
-echo Creating SHA512 value for %BINARY-STANDALONE-TAR%
-gpg2 --print-md SHA512 %BINARY-STANDALONE-TAR% > %BINARY-STANDALONE-TAR%.sha
+gpg2 --armor --output $BINARY-STANDALONE-TAR$.asc --detach-sig $BINARY-STANDALONE-TAR$
+echo verifying the signature for $BINARY-STANDALONE-TAR$
+gpg2 --verify $BINARY-STANDALONE-TAR$.asc $BINARY-STANDALONE-TAR$
+echo Creating MD5 value for $BINARY-STANDALONE-TAR$
+gpg2 --print-md MD5 $BINARY-STANDALONE-TAR$ > $BINARY-STANDALONE-TAR$.md5
+echo Creating SHA512 value for $BINARY-STANDALONE-TAR$
+gpg2 --print-md SHA512 $BINARY-STANDALONE-TAR$ > $BINARY-STANDALONE-TAR$.sha
 
-gpg2 --armor --output %BINARY-STANDALONE-ZIP%.asc --detach-sig %BINARY-STANDALONE-ZIP%
-echo verifying the signature for %BINARY-STANDALONE-ZIP%
-gpg2 --verify %BINARY-STANDALONE-ZIP%.asc %BINARY-STANDALONE-ZIP%
-echo Creating MD5 value for %BINARY-STANDALONE-ZIP%
-gpg2 --print-md MD5 %BINARY-STANDALONE-ZIP% > %BINARY-STANDALONE-ZIP%.md5
-echo Creating SHA512 value for %BINARY-STANDALONE-ZIP%
-gpg2 --print-md SHA512 %BINARY-STANDALONE-ZIP% > %BINARY-STANDALONE-ZIP%.sha
+gpg2 --armor --output $BINARY-STANDALONE-ZIP$.asc --detach-sig $BINARY-STANDALONE-ZIP$
+echo verifying the signature for $BINARY-STANDALONE-ZIP$
+gpg2 --verify $BINARY-STANDALONE-ZIP$.asc $BINARY-STANDALONE-ZIP$
+echo Creating MD5 value for $BINARY-STANDALONE-ZIP$
+gpg2 --print-md MD5 $BINARY-STANDALONE-ZIP$ > $BINARY-STANDALONE-ZIP$.md5
+echo Creating SHA512 value for $BINARY-STANDALONE-ZIP$
+gpg2 --print-md SHA512 $BINARY-STANDALONE-ZIP$ > $BINARY-STANDALONE-ZIP$.sha
 
-cd ../../%BINARY-FOLDER-WAR%
+cd ../../$BINARY-FOLDER-WAR$
 
-gpg2 --armor --output %BINARY-WAR-TAR%.asc --detach-sig %BINARY-WAR-TAR%
-echo verifying the signature for %BINARY-WAR-TAR%
-gpg2 --verify %BINARY-WAR-TAR%.asc %BINARY-WAR-TAR%
-echo Creating MD5 value for %BINARY-WAR-TAR%
-gpg2 --print-md MD5 %BINARY-WAR-TAR% > %BINARY-WAR-TAR%.md5
-echo Creating SHA512 value for %BINARY-WAR-TAR%
-gpg2 --print-md SHA512 %BINARY-WAR-TAR% > %BINARY-WAR-TAR%.sha
+gpg2 --armor --output $BINARY-WAR-TAR$.asc --detach-sig $BINARY-WAR-TAR$
+echo verifying the signature for $BINARY-WAR-TAR$
+gpg2 --verify $BINARY-WAR-TAR$.asc $BINARY-WAR-TAR$
+echo Creating MD5 value for $BINARY-WAR-TAR$
+gpg2 --print-md MD5 $BINARY-WAR-TAR$ > $BINARY-WAR-TAR$.md5
+echo Creating SHA512 value for $BINARY-WAR-TAR$
+gpg2 --print-md SHA512 $BINARY-WAR-TAR$ > $BINARY-WAR-TAR$.sha
 
-gpg2 --armor --output %BINARY-WAR-ZIP%.asc --detach-sig %BINARY-WAR-ZIP%
-echo verifying the signature for %BINARY-WAR-ZIP%
-gpg2 --verify %BINARY-WAR-ZIP%.asc %BINARY-WAR-ZIP%
-echo Creating MD5 value for %BINARY-WAR-ZIP%
-gpg2 --print-md MD5 %BINARY-WAR-ZIP% > %BINARY-WAR-ZIP%.md5
-echo Creating SHA512 value for %BINARY-WAR-ZIP%
-gpg2 --print-md SHA512 %BINARY-WAR-ZIP% > %BINARY-WAR-ZIP%.sha
+gpg2 --armor --output $BINARY-WAR-ZIP$.asc --detach-sig $BINARY-WAR-ZIP$
+echo verifying the signature for $BINARY-WAR-ZIP$
+gpg2 --verify $BINARY-WAR-ZIP$.asc $BINARY-WAR-ZIP$
+echo Creating MD5 value for $BINARY-WAR-ZIP$
+gpg2 --print-md MD5 $BINARY-WAR-ZIP$ > $BINARY-WAR-ZIP$.md5
+echo Creating SHA512 value for $BINARY-WAR-ZIP$
+gpg2 --print-md SHA512 $BINARY-WAR-ZIP$ > $BINARY-WAR-ZIP$.sha
 pause
 
