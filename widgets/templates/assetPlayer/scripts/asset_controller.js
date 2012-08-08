d5db8c723cdf57e03ae718c06d10b7c1db85ed5c
Ross Duncan Gardler <rgardler@apache.org>
2012-08-08 11:50:22 +0000

    add an asset player template - provides basic functionality for displaying image slideshows and audio/video assets
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wookie/trunk@1370743 13f79535-47bb-0310-9956-ffa450edef68

---
+++ widgets/templates/assetPlayer/scripts/asset_controller.js
@@ -0,0 +1,87 @@
+/**
+ * This provides the code for managing assets and collections of assets.
+ * It provides generic functionality that is applicable to all asset types
+ * and is therefore intended to be reused across all asset players.
+ */
+var ${widget.shortname}_asset_controller = { 
+    idx: 0, // index of current asset being displayed,
+    auto: false, // indicates if we are auto playing assets
+    staticDuration: 5000, // interval between auto play of static assets (e.g. photo's)
+    interval: null, // the interval object that controls the auto player
+
+    init:function() { 
+	// register button events
+	$('#next').click(function() {
+	    ${widget.shortname}_asset_controller.nextAsset();
+	});
+
+	$('#prev').click(function() {
+	    ${widget.shortname}_asset_controller.prevAsset();
+	});
+
+	$('#gotoAlbum').click(function() {
+	    alert("gotoAlbum not implemented yet");
+	});
+
+	$('#playPause').click(function() {
+	    if (!${widget.shortname}_asset_controller.auto) {
+		${widget.shortname}_asset_controller.startAuto();
+		// FIXME: this breaks layout - as it overwrides JQuery markup
+		this.innerText = "Pause";
+	    } else {
+		${widget.shortname}_asset_controller.stopAuto();
+		// FIXME: this breaks layout - as it overwrides JQuery markup
+		this.innerText = "Play";
+	    }
+	});
+    },
+    
+    setCollection:function(album){
+	sessionStorage.setItem('collection', JSON.stringify(album));
+        // FIXME: cache images for faster slideshow. e.g. http://www.anthonymclin.com/code/7-miscellaneous/98-on-demand-image-loading-with-jquery
+	${widget.shortname}_asset_controller.displayAsset(0);
+    },
+
+    getCollection:function() {
+	return JSON.parse(sessionStorage.getItem('collection'));
+    },
+    
+    displayAsset:function(idx) {
+	var album = ${widget.shortname}_asset_controller.getCollection();
+	$('#asset').attr("src", album[idx].src);
+    },
+
+    nextAsset:function() {
+	var album = ${widget.shortname}_asset_controller.getCollection();
+	${widget.shortname}_asset_controller.idx += 1;
+        if (${widget.shortname}_asset_controller.idx >= album.length) { 
+	    ${widget.shortname}_asset_controller.idx = 0;
+	};
+	${widget.shortname}_asset_controller.displayAsset(${widget.shortname}_asset_controller.idx);
+    },
+
+    prevAsset:function() {
+	var album = ${widget.shortname}_asset_controller.getCollection();
+	${widget.shortname}_asset_controller.idx -= 1;
+        if (${widget.shortname}_asset_controller.idx <0) { 
+	    ${widget.shortname}_asset_controller.idx = album.length - 1;
+	};
+	${widget.shortname}_asset_controller.displayAsset(${widget.shortname}_asset_controller.idx);
+    },
+
+    startAuto:function() {
+	${widget.shortname}_asset_controller.auto = true;
+	${widget.shortname}_asset_controller.interval = 
+	    window.setInterval(${widget.shortname}_asset_controller.nextAsset, 
+			       ${widget.shortname}_asset_controller.staticDuration);
+    },
+
+    stopAuto:function() {
+	${widget.shortname}_asset_controller.auto = false;
+	window.clearInterval(${widget.shortname}_asset_controller.interval);
+    }
+};
+
+$('#home').live('pageshow',function(event) { 
+  ${widget.shortname}_asset_controller.init(); 
+});
