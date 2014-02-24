653c86d99021101160047776ff6441b844eb109f
Scott Wilson <scottbw@apache.org>
2014-02-24 19:09:29 +0000

    Removed unused import
    git-svn-id: https://svn.apache.org/repos/asf/wookie/trunk@1571401 13f79535-47bb-0310-9956-ffa450edef68

--- wookie-connector/java/src/main/java/org/apache/wookie/connector/framework/SignedApiRequest.java
+++ wookie-connector/java/src/main/java/org/apache/wookie/connector/framework/SignedApiRequest.java
@@ -20,7 +20,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
